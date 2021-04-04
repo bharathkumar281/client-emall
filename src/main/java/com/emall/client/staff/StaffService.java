7cb6ffddfc30eea783211db2a0a4ab226f146e77
bharathkumar281 <munagaribharath1999@gmail.com>
2021-04-04 16:18:38 +0530

    added login

--- src/main/java/com/emall/client/staff/StaffService.java
+++ src/main/java/com/emall/client/staff/StaffService.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Transactional
 @CrossOrigin
 public class StaffService {
-	
+
 	@Autowired
 	private StaffRepository staffRepository;
 
@@ -27,23 +27,35 @@ public class StaffService {
 	public Iterable<Staff> getStaff() {
 		return staffRepository.findAll();
 	}
-	
+
 	@PostMapping(path = "/add")
 	public Staff addStaff(@RequestBody Staff newStaff) {
-		for(Staff staff: staffRepository.findAll()) {
-			if(staff.getEmail().equals(newStaff.getEmail())) return null;
+		for (Staff staff : staffRepository.findAll()) {
+			if (staff.getEmail().equals(newStaff.getEmail()))
+				return null;
 		}
 		return staffRepository.save(newStaff);
 	}
-	
+
 	@GetMapping(path = "/from-mall")
 	public List<Staff> findByMall(@RequestParam Integer id) {
 		return staffRepository.findByMallId(id);
 	}
-	
+
 	@DeleteMapping(path = "/delete-by-mall")
 	public String deleteByMallId(@RequestParam Integer id) {
 		staffRepository.deleteByMallId(id);
 		return "deleted successfully";
+	}
+
+	@PostMapping(path = "/login")
+	public Staff loginValidation(@RequestBody Staff user) {
+
+		for (Staff s : staffRepository.findAll()) {
+			if (s.getEmail().equals(user.getEmail()) && s.getPassword().equals(user.getPassword())) {
+				return s;
+			}
+		}
+		return null;
 	}
 }
